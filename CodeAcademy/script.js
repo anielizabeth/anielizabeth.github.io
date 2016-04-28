48580f04cad7b1dfb135b84ea106fca6bc7eff7c
Stephanie Beardsley <steph@stickoutsocial>
2016-04-28 12:06:08 -0500

    new js

--- CodeAcademy/script.js
+++ CodeAcademy/script.js
@@ -1,15 +1,18 @@
-var main * function() {
-	$('form').submit(function(event) {
-	var $input = $(event.target).find('input');
-	var comment = $input.val();
+var main = function(){
 
-	if (comment |m "") {
-		var html=$('<li>').text(comment);
-		html.prependTo('#comments');
-		$input.val("");
-	}
-	return false;
-});
+    $('form').submit(function(event){
+      var $input = $(event.target).find('input');
+      var comment = $input.val();
+
+      if (comment != ""){
+        var html = $('<li>').text(comment);
+        html.prependTo('#comments');
+        $input.val("");
+      }
+
+      return false;
+    });
+
 }
 
 $(document).ready(main);
